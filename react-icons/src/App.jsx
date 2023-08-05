70982ff019fc9fb3ac001e4745ac3bc795a748eb
Vladimir Hanhur <aqgz@email.cz>
2023-08-05 22:25:43 +0200

    update

---
+++ react-icons/src/App.jsx
@@ -0,0 +1,12 @@
+// import OneMovieSlider from "./components/OneMovieSlider";
+import TwoMovieSlider from "./components/TwoMovieSlider";
+
+const App = () => {
+    return (
+        <div className="slider">
+            {/* <OneMovieSlider /> */}
+            <TwoMovieSlider />
+        </div>
+    );
+}
+export default App;
