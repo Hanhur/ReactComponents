9a05249bfe2889ef5a0cd95bfddb5897beec257d
Vladimir Hanhur <aqgz@email.cz>
2023-12-10 16:40:47 +0100

    update

--- form-advanced/src/App.jsx
+++ form-advanced/src/App.jsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 
 const App = () => {
-    const [fullName, setFullName] = useState("");
-    const [email, setEmail] = useState("");
-    const [age, setAge] = useState("");
+    const [oneUser, setOneUser] = useState({
+        fullName: "",
+        email: "",
+        age: ""
+    });
     const [users, setUsers] = useState([]);
+
+    const formChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setOneUser({
+            ...oneUser,
+            [name]: value,
+        });
+    }
 
     const formSubmit = (event) => {
         event.preventDefault();
         
-        if(fullName && email) 
+        if(oneUser.fullName && oneUser.email && oneUser.age) 
         {
-            const oneUser = {fullName: fullName, email: email, age: age}
+            const newUser = {
+                ...oneUser,
+                id: new Date().getTime(),
+            }
             setUsers((users) => {
-                return [...users, oneUser];
+                return [...users, newUser];
+            });
+
+            setOneUser({
+                fullName: "",
+                email: "",
+                age: ""
             });
         } 
         else 
         {
             console.log("Něco nebylo vyplněno!");
         }
-        setFullName("");
-        setEmail("");
-        setAge("");
     }
 
     return (
@@ -32,30 +50,33 @@ const App = () => {
                     className="userInfo" 
                     type="text" 
                     placeholder="Name"
-                    value={fullName}
-                    onChange={(event) => setFullName(event.target.value)}
+                    value={oneUser.fullName}
+                    onChange={formChange}
+                    name="fullName"
                 />
                 <input 
                     className="userInfo" 
                     type="email" 
                     placeholder="Email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
+                    value={oneUser.email}
+                    onChange={formChange}
+                    name="email"
                 />
                 <input
                     className="userInfo"
                     type="text"
                     placeholder="Vek"
-                    value={age}
-                    onChange={(event) => setAge(event.target.value)}
+                    value={oneUser.age}
+                    onChange={formChange}
+                    name="age"
                 />
                 <input type="submit"/>
             </form>
             {
-                users.map((oneUser, index) => {
-                    const {fullName, email, age} = oneUser;
+                users.map((oneUser) => {
+                    const {id, fullName, email, age} = oneUser;
                     return (
-                        <div className="item" key={index}>
+                        <div className="item" key={id}>
                             <h4>{fullName}</h4>
                             <p>{email}</p>
                             <p>{age}</p>
