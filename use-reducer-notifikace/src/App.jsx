47fd0310298f5697f745cf38843ac5d3ed228baf
Vladimir Hanhur <aqgz@email.cz>
2023-12-10 23:21:46 +0100

    update

--- use-reducer-notifikace/src/App.jsx
+++ use-reducer-notifikace/src/App.jsx
@@ -1,11 +1,57 @@
+import Modal from "./components/Modal";
+import data from "./data";
+import { useState, useReducer } from "react";
 
+const reducer = () => {
+
+}
+
+const defaultState = {
+    movies: [],
+    showNotification: false,
+    notificationContent: ""
+}
 
 const App = () => {
-    return (
-        <div>
+    // const [movies, setMovies] = useState(data);
+    // const [showNotification, setShowNotification] = useState(false);
+    const [movieName, setMovieName] = useState("");
+    const [state, dispatch] = useReducer(reducer, defaultState);
 
-        </div>
+    const submitForm = (event) => {
+        event.preventDefault();
+
+        if(movieName) 
+        {
+            
+        }
+        else 
+        {
+            
+        }
+    }
+
+    return (
+        <section>
+            {state.showNotification && <Modal notifContent={state.notificationContent}/>}
+            <form onSubmit={submitForm}>
+                <input type="text" onChange={(event) => setMovieName(event.target.value)}/>
+                <input type="submit" value="Pridat"/>
+            </form>
+            <div>
+                {
+                    state.movies.map((oneMovie) => {
+                        return (
+                            <div key={oneMovie.id}>
+                                <p>{oneMovie.name}</p>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </section>
     );
 }
 
 export default App;
+// 44
---
+++ use-reducer-notifikace/src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import './Modal.css';
+
+const Modal = ({notifConten}) => {
+    return (
+        <div>
+            {notifConten}
+        </div>
+    );
+}
+
+export default Modal;
