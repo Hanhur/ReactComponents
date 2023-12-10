0e653d1da5b8a6896fcca43eae3843bd37918c3e
Vladimir Hanhur <aqgz@email.cz>
2023-12-10 17:59:33 +0100

    update

---
+++ lorem-ipsum-generator/src/App.jsx
@@ -0,0 +1,55 @@
+import data from './data';
+import { useState } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [paragraphs, setParagraphs] = useState([]);
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        let amount = parseInt(count);
+
+        if(amount < 0) 
+        {
+            amount = 1;
+        }
+        else if(amount > 5) 
+        {
+            amount = 5;
+        }
+
+        // const resultParagraphs = data.filter((oneParagraph, index) => {
+        //     return index < amount;
+        // });
+
+        // setParagraphs(resultParagraphs);
+
+        setParagraphs(data.slice(0, amount));
+    }
+
+    return (
+        <section className="form-section">
+            <h1>Lorem ipsum generator</h1>
+            <form onSubmit={submitForm}>
+                <label htmlFor="paragraphs">Odstavce: </label>
+                <input 
+                    type="number" 
+                    id="paragraphs" 
+                    onChange={(event) => setCount(event.target.value)}
+                /><br/>
+                <input type="submit" value="Vygenerovat"/>
+            </form>
+            <article>
+                {
+                    paragraphs.map((oneParagraph, index) => {
+                        return (
+                            <p key={index}>{oneParagraph}</p>
+                        );
+                    })
+                }
+            </article>
+        </section>
+    );
+}
+
+export default App;
