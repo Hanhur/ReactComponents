c3eef6d46c882e5018db2d7c4b91fbcf0e94a281
Vladimír Hanhur <aqgz@email.cz>
2025-09-21 17:44:28 +0200

    update

---
+++ lorem-generator/src/App.js
@@ -0,0 +1,44 @@
+import data from "./data";
+import { useState } from "react";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [paragraphs, setParagraphs] = useState([]);
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        let amount = parseInt(count);
+
+        // kontrola krajních hodnot 
+        if (amount < 0) { 
+            amount = 1;
+        } else if (amount > 5) { 
+            amount = 5; 
+        }
+
+        setParagraphs(data.slice(0, amount));
+    }
+
+    return (
+        <section className="form-section">
+            <h1>Lorem ipsum generátor</h1>
+            <form onSubmit={submitForm}>
+                <label htmlFor="paragraphs">Odstavce:</label>
+                <input type="number" id="paragraphs" onChange={(event) => setCount(event.target.value)} /><br />
+                <input type="submit" value="Vygenerovat" />
+            </form>
+            <article>
+                {
+                    paragraphs.map((oneParagraph, index) => {
+                        return (
+                            <p key={index}>{oneParagraph}</p>
+                        );
+                    })
+                }
+            </article>
+        </section>
+    );
+}
+
+export default App;
+// lesson 30 / 49
