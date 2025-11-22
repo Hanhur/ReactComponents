521c4653ef29002c34f5c0677375da2b4bbb9c70
Vladimír Hanhur <aqgz@email.cz>
2025-11-22 20:44:14 +0100

    update

---
+++ use-reducer-notifikace/src/components/Modal.jsx
@@ -0,0 +1,16 @@
+import "./Modal.css";
+import { useEffect } from "react";
+
+const Modal = ({notifContent, closeNotif}) => {
+    useEffect( () => {
+        setTimeout( () => {
+            closeNotif();
+        }, 2000);
+    });
+
+    return (
+        <div className="notification">{notifContent}</div>
+    );
+}
+
+export default Modal;
