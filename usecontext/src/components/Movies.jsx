07f5aaa9956c266b16c77e74f2e223ba47d0ab5d
Vladimír Hanhur <aqgz@email.cz>
2026-02-06 10:15:56 +0100

    update

---
+++ usecontext/src/components/Movies.jsx
@@ -0,0 +1,25 @@
+import MoviesContext from "./MoviesContext";
+import { useContext } from "react";
+
+const Movies = ({}) => {
+
+    const {movies, deleteMovie} = useContext(MoviesContext);
+
+    return (
+        <div>
+            {
+                movies.map((oneMovie) => {
+                    const {id, name} = oneMovie;
+                    return(
+                        <div key={id}>
+                            <p>{name}</p>
+                            <button onClick={() => deleteMovie(id)}>Smazat</button>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default Movies;
